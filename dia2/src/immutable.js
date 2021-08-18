467caa3d6b7eb4f527ff21364133e464f7abeeb9
IIcobus2 <walter_felizardo@live.com>
2021-08-18 12:05:54 -0300

    Excercise 1 - Immutability - DONE

--- dia2/src/immutable.js
+++ dia2/src/immutable.js
@@ -5,10 +5,10 @@ const john = {
   hobbies: ['Surf', 'Design'],
 }
 
-const jane = john
-
-jane.name = 'Jane'
-jane.hobbies.push('MuayThai', 'Programming')
+const jane = Object.assign({}, john); {
+  jane.name = 'Jane';
+  jane.hobbies = ['MuayThai', 'Programming'];
+}
 
 console.log('John:', john)
 console.log('Jane:', jane)
